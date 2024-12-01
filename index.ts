0430f4b5e721c0974b7b79bf2f8d800d42fa9dd9
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2024-12-01 11:41:44 +0600

    Application logic changed without using FileDb

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import messagesRouter from "./routes/messages";
 import fs = require('fs');
-import fileDb from "./FileDb";
 
 const app = express();
 const port = 8000;
@@ -9,12 +8,9 @@ app.use(express.json());
 app.use('/messages', messagesRouter);
 
 const run = async () => {
-    if(fs.existsSync('./messages')){
-        await fileDb.init();
-    } else{
+    if(!fs.existsSync('./messages')){
         fs.mkdir('./messages', (err) => {console.log(err)});
     }
-
     app.listen(port, () => {
         console.log(`Server started on http://localhost:${port} port!`);
     });
--- routes/messages.ts
+++ routes/messages.ts
@@ -1,13 +1,30 @@
 import express from "express";
-import fileDb from "../FileDb";
 import {Message, MessageWithoutData} from "../types";
 import {promises as fs} from "fs";
 
 const messagesRouter = express.Router();
 
 messagesRouter.get('/', async (req, res) => {
-    const messages = await fileDb.getItems();
-    res.send(messages);
+        const path = './messages';
+        const fileArray:Message[] = [];
+        const data = await fs.readdir(path);
+        const readMessageFile = async (fileName:string) => {
+            try {
+                const fileContents = await fs.readFile(fileName);
+                const result:Message = await JSON.parse(fileContents.toString());
+                fileArray.push(result);
+                return fileArray;
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        for(let i=data.length-1 ; i>=0; i--){
+            if(fileArray.length<5){
+                await readMessageFile(path + '/' + data[i]);
+            }
+            else {break}
+        }
+    res.send(fileArray.reverse());
 });
 
 messagesRouter.post('/create', async (req, res) => {
@@ -15,10 +32,9 @@ messagesRouter.post('/create', async (req, res) => {
         message: req.body.message,
     };
     const date = new Date().toISOString();
-    const messageNew:Message = {date, ...message}
+    const messageNew:Message = {date, ...message};
     await fs.writeFile(`./messages/${date}.txt`, JSON.stringify(messageNew));
     res.send(messageNew);
-
 });
 
 export default messagesRouter;
